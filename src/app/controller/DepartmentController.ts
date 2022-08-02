2af8108756699d3e1ad3a3a51de6a31310e07e0d
sreeraj <sreerajpr06@gmail.com>
2022-08-02 23:36:48 +0530

    add authorization for department crud operations

--- src/app/controller/DepartmentController.ts
+++ src/app/controller/DepartmentController.ts
@@ -1,13 +1,14 @@
 import { AbstractController } from "../util/rest/controller";
 import { NextFunction, Response } from "express";
 import RequestWithUser from "../util/rest/request";
-import APP_CONSTANTS from "../constants";
+import APP_CONSTANTS, { USER_ROLES } from "../constants";
 import { DepartmentService } from "../service/DepartmentService";
 import { Department } from "../entities/Department";
 import validationMiddleware from "../middleware/ValidationMiddleware";
 import { CreateDepartmentDto } from "../dto/CreateDepartment";
 import { UuidDto } from "../dto/Uuid";
 import { UpdateDepartmentDto } from "../dto/UpdateDepartment";
+import authorize from "../middleware/Authorize";
 
 class DepartmentController extends AbstractController {
   constructor(private departmentService: DepartmentService) {
@@ -18,26 +19,31 @@ class DepartmentController extends AbstractController {
   protected initializeRoutes() {
     this.router.get(
       `${this.path}`, 
+      authorize([USER_ROLES.admin, USER_ROLES.developer, USER_ROLES.engineer, USER_ROLES.guest, USER_ROLES.manager]),
       this.getDepartments
     );
     this.router.get(
       `${this.path}/:id`,
+      authorize([USER_ROLES.admin, USER_ROLES.manager, USER_ROLES.developer, USER_ROLES.engineer]),
       validationMiddleware(UuidDto, APP_CONSTANTS.params),
       this.getDepartmentById
     );
     this.router.put(
       `${this.path}/:id`,
+      authorize([USER_ROLES.admin, USER_ROLES.manager]),
       validationMiddleware(UuidDto, APP_CONSTANTS.params),
       validationMiddleware(UpdateDepartmentDto, APP_CONSTANTS.body),
       this.updateDepartment
     );
     this.router.post(
       `${this.path}`,
+      authorize([USER_ROLES.admin, USER_ROLES.manager]),
       validationMiddleware(CreateDepartmentDto, APP_CONSTANTS.body),
       this.createDepartment
     );
     this.router.delete(
       `${this.path}/:id`,
+      authorize([USER_ROLES.admin]),
       validationMiddleware(UuidDto, APP_CONSTANTS.params),
       this.deleteDepartment
     )
