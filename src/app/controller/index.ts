0f1669e64541ab8f23832fecc903376416539078
sreeraj <sreerajpr06@gmail.com>
2022-08-02 10:28:05 +0530

    Update for department crud operation

--- src/app/controller/index.ts
+++ src/app/controller/index.ts
@@ -8,9 +8,13 @@ import { EmployeeRepository } from "../repository/EmployeeRepository";
 import DepartmentController from "./DepartmentController";
 import { DepartmentService } from "../service/DepartmentService";
 import { DepartmentRepository } from "../repository/DepartmentRepository";
+// import AddressController from "./AddressController";
+// import { AddressService } from "../service/AddressService";
+// import { AddressRepository } from "../repository/AddressRepository";
 
 export default [
   new HealthController(),
   new EmployeeController(new EmployeeService(new EmployeeRepository())),
-  new DepartmentController(new DepartmentService(new DepartmentRepository()))
+  new DepartmentController(new DepartmentService(new DepartmentRepository())),
+  // new AddressController(new AddressService(new AddressRepository()))
 ];
